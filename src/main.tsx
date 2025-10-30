c3245560e6d546c70b3a935f76bd0490324d6b7b
Derfolem <102155210+Derfolem@users.noreply.github.com>
2025-10-29 23:20:55 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,3 +3,6 @@ import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
+import Contato from "@/pages/Contato";
+// ...
+<Route path="/contato" element={<Contato />} />
